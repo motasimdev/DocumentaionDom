118ee8ba6ee0603d2f0aad5e9ec8d5e66ab334cb
motasimdev <themotasimdev@gmail.com>
2025-05-15 12:09:30 +0600

    aaa

--- js/script.js
+++ js/script.js
@@ -3,13 +3,3 @@
 // array like object : vitorer lekha dhorte hole [2].innerHTML lekhte hbe
 
 
-let btn = document.getElementById("btn")
-let reviewBox = document.getElementById("reviewBox")
-let para = document.querySelector("p")
-
-
-btn.addEventListener("click", function () {
-    
-    para.innerHTML = reviewBox.value
-    
-})
