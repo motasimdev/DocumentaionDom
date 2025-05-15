8f0b098bcf4d4fb54d1bbf01a22e6dea869d676c
motasimdev <themotasimdev@gmail.com>
2025-05-15 12:11:47 +0600

    ss

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@
 // array like object : vitorer lekha dhorte hole [2].innerHTML lekhte hbe
 
 
+let heading = document.getElementById("heading")
+let inputDlt = document.getElementById("inputDlt")
+let dltBtn = document.getElementById("dltBtn")
+
+dltBtn.addEventListener("click", ()=>{
+    heading.style.display= "none"
+})
